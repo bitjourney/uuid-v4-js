e490bc776a09e5b45a8fdabec80b54875727be0f
FUJI Goro (gfx) <gfuji@cpan.org>
2018-04-12 18:44:27 +0900

    add tests

---
+++ test/uuid-v4.test.ts
@@ -0,0 +1,9 @@
+import { generateUUIDv4 } from "../src/uuid-v4";
+import isUUID from "is-uuid";
+
+
+console.log("1..10");
+for (let i = 0; i < 10; i++) {
+  const uuid = generateUUIDv4();
+  console.log((isUUID.v4(uuid) ? "ok " : "not ok ") + (i + 1));
+}
